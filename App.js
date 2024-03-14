d7372e31af7c063063ebd8a6208693bc97151022
AmitDaware <88260530+AmitDaware@users.noreply.github.com>
2024-03-14 15:04:48 +0530

    episode 01

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
-// const heading = document.createElement("h1");
-// heading.innerHTML = 'HELLO WORLD FORM JAVASCRIPT !'
-
-// const root = document.getElementById('root');
-// root.appendChild(heading);
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "heading" }, "iam nested h1 tag")
-  )
-);
-
-console.log(parent);
+// part one  start----------------------------
 
 // const heading = React.createElement(
 //   "h1",
-//   { id: "heading", class: "head" },
-//   "Hello World form react"
+//   { id: "heading", class: "head" },//this object is used for  giving attributes to our elements.
+//   "Hello World form react!"
 // );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading)
+
+//part one end ----------------------------------
+
+// part TWO start-------------creating the nested HTML structure inside react ------------------
+
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1 id="heading">Iam a h1 tag</h1>
+    <h2 id="heading2">Iam a h2 tag</h2>
+  </div>
+</div>; */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },[ React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "Iam a h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "Iam a h2 tag"),
+  ])],
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "Iam a h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "Iam a h2 tag"),
+  ])
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Namaste React 💻
+
+# Parcel
+- Dev Build
+- Local Server
+- Hot Module Replacement (HMR)
+- File Watching Algorithm -  written in c++
+- caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support older browsers
+- Diagnostics
+- Error Handling
