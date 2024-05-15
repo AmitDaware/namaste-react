c75ec45ab242db14c84d98db0a0f1039672320d3
AmitDaware <88260530+AmitDaware@users.noreply.github.com>
2024-05-15 14:32:55 +0530

    Add .gitattributes for automatic line ending handling

--- App.js
+++ App.js
@@ -15,25 +15,26 @@
 // part TWO start-------------creating the nested HTML structure inside react ------------------
 
 {
-  /* <div id="parent">
-  <div id="child">
-    <h1 id="heading">Iam a h1 tag</h1>
-    <h2 id="heading2">Iam a h2 tag</h2>
-  </div>
-</div>; */
-}
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },[ React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading" }, "Iam a h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "Iam a h2 tag"),
-  ])],
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "Iam a h1 tag"),
-    React.createElement("h2", { id: "heading2" }, "Iam a h2 tag"),
-  ])
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+    /* <div id="parent">
+    <div id="child">
+      <h1 id="heading">Iam a h1 tag</h1>
+      <h2 id="heading2">Iam a h2 tag</h2>
+    </div>
+  </div>; */
+  }
+  
+  const parent = React.createElement(
+    "div",
+    { id: "parent" },[ React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", { id: "heading" }, "Iam a h1 tag"),
+      React.createElement("h2", { id: "heading2" }, "Iam a h2 tag"),
+    ])],
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", { id: "heading" }, "Iam a h1 tag"),
+      React.createElement("h2", { id: "heading2" }, "Iam a h2 tag"),
+    ])
+  );
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
+  
