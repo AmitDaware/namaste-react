a8bb9bf644b24d3fbcce54fbc473addb57d8fbc1
AMIT SURESH DAWARE <88260530+AmitDaware@users.noreply.github.com>
2024-07-26 15:26:25 +0530

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,13 @@
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" ;
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const LOGO_URL = "https://cdn.vectorstock.com/i/500p/70/45/kebab-house-vintage-colorful-label-vector-46457045.jpg";
+export const LOGO_URL =
+  "https://cdn.vectorstock.com/i/500p/70/45/kebab-house-vintage-colorful-label-vector-46457045.jpg";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.481785&lng=73.9168766&restaurantId=";
+
+export const RES_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.481785&lng=73.9168766&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+// corsproxy cha site varuna https://corsproxy.io/? evdha copy karun https chy samor takne mhanje CORS chy extension chi garaj nahi padnar. but tya sathi vpn connect karava lagte..
